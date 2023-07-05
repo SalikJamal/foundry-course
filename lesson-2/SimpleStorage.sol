92bad9e62124b79b6a0207e6766b82fc57703b1a
Salik <iamsalikjamal@gmail.com>
2023-07-05 16:38:30 +0500

    Lesson 2 Part 4 done

--- lesson-2/SimpleStorage.sol
+++ lesson-2/SimpleStorage.sol
@@ -5,6 +5,20 @@ pragma solidity ^0.8.18;
 
 contract SimpleStorage {
 
-    uint256 favoriteNumber; 
+    uint256 public favoriteNumber; 
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // View function
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+    
+    // Pure function
+    function pureFunction() public pure returns(uint256) {
+        return 5 + 5;
+    }
 
 }
