0a7c1410b349fd62bc3b4384c27daec495a7691f
Salik <iamsalikjamal@gmail.com>
2023-07-06 15:25:22 +0500

    Lesson 2 Part 5

--- lesson-2/SimpleStorage.sol
+++ lesson-2/SimpleStorage.sol
@@ -5,20 +5,28 @@ pragma solidity ^0.8.18;
 
 contract SimpleStorage {
 
-    uint256 public favoriteNumber; 
+    uint256 myFavoriteNumber; // 0
+    
+    // uint256[] listOfFavoriteNumbers;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople; // []
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     // View function
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
-    
-    // Pure function
-    function pureFunction() public pure returns(uint256) {
-        return 5 + 5;
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
     }
 
 }
