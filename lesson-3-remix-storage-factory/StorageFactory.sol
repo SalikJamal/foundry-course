34eef663a777f7da26324f7befd117b3392f2406
Salik <iamsalikjamal@gmail.com>
2023-07-08 18:50:41 +0500

    Compeleted lesson 3 part 5

--- lesson-3-remix-storage-factory/StorageFactory.sol
+++ lesson-3-remix-storage-factory/StorageFactory.sol
@@ -2,36 +2,8 @@
 
 pragma solidity ^0.8.18;
 
+import { SimpleStorage } from "./SimpleStorage.sol";
 
-contract SimpleStorage {
-
-    uint256 myFavoriteNumber;
-    
-    struct Person {
-        uint256 favoriteNumber;
-        string name;
-    }
-
-    Person[] public listOfPeople;
-
-    mapping(string => uint256) public nameToFavoriteNumber;
-
-    function store(uint256 _favoriteNumber) public {
-        myFavoriteNumber = _favoriteNumber;
-    }
-
-    // View function
-    function retrieve() public view returns(uint256) {
-        return myFavoriteNumber;
-    }
-
-    // Calldata, memory, storage
-    function addPerson(uint256 _favoriteNumber, string calldata _name) public {
-        listOfPeople.push(Person(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-
-}
 
 contract StorageFactory {
 
