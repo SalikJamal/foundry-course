b37a2907fde107a6388446a3cf4850047ff36161
Salik <iamsalikjamal@gmail.com>
2023-07-10 22:59:50 +0500

    Completed Lesson 3 part 7

---
+++ lesson-3-remix-storage-factory/AddFiveStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import { SimpleStorage } from "./SimpleStorage.sol";
+
+contract AddFiveStorage is SimpleStorage {
+
+    function store(uint256 _newNumber) override public {
+        myFavoriteNumber = _newNumber + 5;
+    }
+
+}
