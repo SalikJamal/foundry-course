7175ba392e9a4528f4eae5cc064a2ba63ca55638
Salik <iamsalikjamal@gmail.com>
2023-07-10 22:46:35 +0500

    Completed Lesson 3 part 7

--- lesson-3-remix-storage-factory/StorageFactory.sol
+++ lesson-3-remix-storage-factory/StorageFactory.sol
@@ -7,10 +7,19 @@ import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 
 }
