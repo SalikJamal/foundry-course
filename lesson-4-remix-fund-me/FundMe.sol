538b2400e6ddf1aa424b5f6f93a53131bddf3972
Salik <iamsalikjamal@gmail.com>
2023-07-13 23:02:58 +0500

    Lesson 4 part 2

---
+++ lesson-4-remix-fund-me/FundMe.sol
@@ -0,0 +1,16 @@
+// Get funds from users into this contract
+// Withdraw funds to the owner of this contract
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+contract FundMe {
+
+    function fund() public {}
+
+    // function withdraw() public {}
+
+}
