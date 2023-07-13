f78c4b77b919e55eebd76e6dd5ad5f40361ce170
Salik <iamsalikjamal@gmail.com>
2023-07-13 23:39:33 +0500

    Completed lesson 4 part 5

--- lesson-4-remix-fund-me/FundMe.sol
+++ lesson-4-remix-fund-me/FundMe.sol
@@ -9,10 +9,12 @@ pragma solidity ^0.8.18;
 
 contract FundMe {
 
+    uint256 public minimumUsd = 5;
+
     function fund() public payable {
         // Allow users to send $
-        // Have a minimum $ sent
-        require(msg.value > 1e18, "Didn't sent enough ETH"); // 1e18 = 1 ETH
+        // Have a minimum $5 sent
+        require(msg.value >= minimumUsd, "Didn't sent enough ETH"); // 1e18 = 1 ETH
     }
 
     // function withdraw() public {}
