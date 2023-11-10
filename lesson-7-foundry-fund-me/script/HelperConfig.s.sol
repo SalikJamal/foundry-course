cee5be5001066a638a70d65b94c5d3815c237f61
SalikJamal <iamsalikjamal@gmail.com>
2023-11-10 15:24:30 +0500

    Lesson 7 Part 10, mocking

--- lesson-7-foundry-fund-me/script/HelperConfig.s.sol
+++ lesson-7-foundry-fund-me/script/HelperConfig.s.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import { Script } from 'forge-std/Script.sol';
+import { MockV3Aggregator } from '../test/mocks/MockV3Aggregator.sol';
 
 /**
  * 1. Deploy mocks when we are on a local anvil chain
@@ -15,7 +16,7 @@ import { Script } from 'forge-std/Script.sol';
 */
 
 
-contract HelperConfig {
+contract HelperConfig is Script {
 
     NetworkConfig public activeNetworkConfig;
 
@@ -34,21 +35,27 @@ contract HelperConfig {
     }
 
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory) {
-        // price feed address
         NetworkConfig memory sepoliaConfig = NetworkConfig({ priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 });
         return sepoliaConfig;
     }
 
     function getMainnetEthConfig() public pure returns(NetworkConfig memory) {
-        // price feed address
         NetworkConfig memory mainnetConfig = NetworkConfig({ priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419 });
         return mainnetConfig;
     }
 
-    function getAnvilEthConfig() public pure returns(NetworkConfig memory) {
-        // price feed address
-        // NetworkConfig memory anvilConfig = NetworkConfig({ priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 });
-        // return anvilConfig;
+    function getAnvilEthConfig() public returns(NetworkConfig memory) {
+        
+        // 1. Deploy the mocks on local network
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+            MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({ priceFeed: address(mockPriceFeed) });
+        return anvilConfig;
+        
     }
 
 }
