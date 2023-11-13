204020fa0bb439d93eb9caaae77aa0e531c81ce7
SalikJamal <iamsalikjamal@gmail.com>
2023-11-13 16:29:28 +0500

    Lesson 7 Part 13, More Cheatcodes

--- lesson-7-foundry-fund-me/src/FundMe.sol
+++ lesson-7-foundry-fund-me/src/FundMe.sol
@@ -18,11 +18,11 @@ contract FundMe {
     
     uint256 public constant MIN_USD = 5e18;
     
-    address[] public funders;
+    address[] private s_funders;
 
     address public immutable i_owner;
 
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address funder => uint256 amountFunded) private s_addressToAmountFunded;
 
     AggregatorV3Interface private s_priceFeed;
 
@@ -34,7 +34,9 @@ contract FundMe {
     }
 
     modifier onlyOwner {
-        if(msg.sender != i_owner) { revert FundMe__NotOwner(); }
+        if(msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
         _;
     }
 
@@ -46,27 +48,64 @@ contract FundMe {
         fund();
     }
 
-    function fund() public payable onlyOwner {
+    function fund() public payable {
         require(msg.value.getConversionRate(s_priceFeed) >= MIN_USD, "Didn't sent enough ETH"); // 1e18 = 1 ETH
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function withdraw() public onlyOwner {
-        for(uint funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for(uint funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
 
         // Reset the array
-        funders = new address[](0); // A brand new array with data type address with value to be 0 addresses
-        (bool callSuccess, ) = payable(msg.sender).call{ value: address(this).balance }("");
-        require(callSuccess, "Call Failed");
+        s_funders = new address[](0); // A brand new array with data type address with value to be 0 addresses
+        (bool success, ) = payable(msg.sender).call{ value: address(this).balance }("");
+        require(success, "Call Failed");
+    }
+
+    function cheaperWithdraw() public onlyOwner {
+        address[] memory funders = s_funders;
+
+        // mappings can't be in memory, sorry!
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    /** Getter Functions */
+
+    /**
+     * @notice Gets the amount that an address has funded
+     *  @param fundingAddress the address of the funder
+     *  @return the amount funded
+    */
+    function getAddressToAmountFunded(address fundingAddress) public view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) public view returns(address) {
+        return s_funders[index];
+    }
+
+    function getOwner() public view returns(address) {
+        return i_owner;
+    }
+
+    function getPriceFeed() public view returns(AggregatorV3Interface) {
+        return s_priceFeed;
     }
 
     function getVersion() public view returns(uint256) {
         return s_priceFeed.version();
     }
- 
+
 }
 
--- lesson-7-foundry-fund-me/test/FundMeTest.t.sol
+++ lesson-7-foundry-fund-me/test/FundMeTest.t.sol
@@ -10,11 +10,17 @@ contract FundMeTest is Test {
 
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+
+    uint256 constant SEND_VALUE = 0.1 ether; // 100000000000000000
+    uint256 constant STARTING_BALANCE = 10 ether; // 10000000000000000000
+
     function setUp() external {
         // We deploy FundMeTest contract
         // FundMeTest contract deploys FundMe contract
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinDollarIsFive() public {
@@ -28,6 +34,18 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public {
         uint256 ver = fundMe.getVersion();
         assertEq(ver, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund(); // Send 0 value
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next Tx will be sent by USER
+        fundMe.fund{ value: SEND_VALUE }();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 
 }
